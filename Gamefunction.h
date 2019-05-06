5650b4697f1b98d15c9dbd377caa80fb80fbee47
Serhiy-Ly <serrerttt@gmail.com>
2019-05-06 20:50:50 +0300

    DONE

--- Gamefunction.h
+++ Gamefunction.h
@@ -1,12 +1,11 @@
-//typedef ostream_iterator<string> output_str;
+#include <ctime>
 
 using namespace std;
 
-int m=0;
-int b=0; ///////////////// ПРОБЛЕМА В СЕРЧІНЛОГ
-int n=0;
-int q=0;
-int randomS=0;///////////////////////////////  ШОСЬ ЗРОИТИ З КІЛЬКІСТЮ ІТЕРАЦІЙ
+int m=0;// getword |
+int b=0; // getword |
+int n=0; // rightword |
+int q = 0; // getword |
 
 void rulespintf(string & bufer)
 {
@@ -36,115 +35,85 @@ void ConverttoLow(string & b)// перетворення букв в маленькі
     }
 }
 
-bool searchinLOG(string & word) // Чи є слово в лозі?
-{
-    int i = 0;
-    do{
-    vector<string> wordis;
-    q=0;
-    //i=0;
-    wordis.clear();
-    string line_srch;
-    ifstream LOG("LOGS.txt",ios_base::in);
-    while(!LOG.eof())
-    {
-        q++;
-        getline(LOG,line_srch);
-        wordis.push_back(line_srch);
-        for ( ; i<q+1; i++)
-        {
-            if ( wordis[i] == word )
-            {
-                cout << i <<endl;
-                cout << wordis[i] << endl;
-                cout << " searchinLOG V CIKLI" << endl;
-                i++;
-                return 1;
-            }
-        }
-    }
-    cout << " searchinLOG " << endl;
-    }while (false);
-    return 0;
-}
-
 void LOGAD ( string & word ) // Записати в лог
 {
     ofstream LOG("LOGS.txt",ios_base::out|ios_base::app);
     LOG<<word<<endl;
-    cout << "LOGAD" << endl;
     LOG.close();
 }
 
-int random(int & setsize)
+bool searchinLOG(string & word) // Чи є слово в лозі?
 {
-    srand(time(NULL));
-    rand()%setsize;
-   /* if (rand()%setsize == rand()%setsize)
+    string line_srch="";
+    ifstream LOG("LOGS.txt",ios_base::in);
+    while(!LOG.eof())
     {
-        cout << rand()%setsize << "V CIKLI" << endl;
-        while (rand()%setsize < setsize+1)
+        getline(LOG,line_srch);
+
+        if ( line_srch == word )
         {
-            rand()%setsize++;
+            return 1;
         }
-        random(setsize);
     }
-    //randomS = rand()%setsize;
-    cout << rand()%setsize << " NE V CIKLI" << endl;
-    srand(time(NULL));*/
-    return rand()%(setsize);
+    return 0;
 }
 
-string getword(string & bufer, string & filename,int & setsize,int &i,string &line2)
+string getword(string & bufer, string & filename,int & setsize,string & word,int & ls_lt)
 {
-    n=0; // мітка
-    b=0;
+    m=0; // мітка
+    b=0; // лічильник
 
     string line[80];
-    vector<string> word;
+    vector<string> words;
 
     ifstream file (filename.c_str(), ios_base::in); //відкриваємо файл
+
     while (!file.eof())
     {
         b++;
         for (int i=0; i<b; i++)
         {
             getline(file,line[i]);
-            bufer.length();
-            if  (line[i][0] == bufer[bufer.length()-1]) // чи перша буква зчитаного = останній букві введеного bufer[50][bufer[50].length()
+            bufer.length(); /////////////// якщо -1, то при закінченні на ь, треба щоб було -2 // ЗРОБЛЕНО
+            if  (line[i][0] == bufer[bufer.length() - ls_lt]) // чи перша буква зчитаного = останній букві введеного bufer[50][bufer[50].length()
             {
-                word.push_back(line[i]);
+                words.push_back(line[i]);
             }
         }
     }
 
-    setsize = word.size();
     file.close();
-    label1:
-    line2=word.at(random(setsize));
-   // cout << random(setsize);
-    //cout << line2;
-    n++;
-    cout << "getword" << endl;
-    cout << line2 << endl;
-    //copy(word.begin(), word.end(), output_str(std::cout, "\n") );
-    if (n == 100)
-        line2 = "end";
-    if(searchinLOG(line2) == 1)
+    setsize = words.size();
+    q=0;
+    do
     {
-        goto label1;
-    }
-    word.clear();
-    return line2;
+        q++;
+        word = words.at(rand()% (setsize+0));
+        m++;
+        if (q > 1000)
+        {
+            cout <<"Зачекай я думаю слово..." <<endl;
+            q = -9999999;
+        }
+
+        else if ( m > 300)
+        {
+            word = "end";
+            return word;
+        }
+
+    }while (searchinLOG(word) == 1);
+    words.clear();
+    return word;
 }
 
 
 bool rightword(string & bufer, string & filename)
- {
+{
+    n=0;
     string line;
     vector<string> words;
     words.clear();
-    n=0;
     ifstream file (filename.c_str());
     while (!file.eof() )
     {
@@ -155,16 +124,11 @@ bool rightword(string & bufer, string & filename)
         {
             if (bufer == words[i])
             {
-                cout << "rightword" << endl;
-                //words.clear();
                 return 1;
             }
         }
-
     }
-    cout << "rightword 2" << endl;
-   // words.clear();
-            return 0;
+    return 0;
 }
 
 bool exit(string & bufer)
--- main.cpp
+++ main.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include <windows.h>
-#include <fstream>  // ВВОДИТИ СЛОВА З ПРОБІЛАМИ LOGAD SEARCHINLOG і тд
+#include <windows.h> // SetConsoleOutputCP & SetConsoleCP
+#include <fstream>
 #include <conio.h>
-#include <iterator>
-#include <algorithm>
-#include <set>
 #include <vector>
 #include <ctime>
 #include "Gamefunction.h"
-//using namespace System;
-//using namespace System::Windows::Forms;
 
 using namespace std;
 
-/*[STAThread]
-void main2() {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Project2::MyForm form;
-	Application::Run(%form);
-*/
-
-
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251); // Ввід кирилицею
+    SetConsoleOutputCP(1251);// Вивід кирилицею
 
-    ofstream LOG("LOGS.txt");
-    LOG.close();
-
-    ifstream file;//потоки работы с файлами
+    ofstream LOG("LOGS.txt"); // Ств файл
+    LOG.close();// Зберігаємо файл
+    //потоки роботи з файлами
+    ifstream file;
     ofstream fout;
 
-   // cout << "\t Привіт!!\nСьогодні ми зіграємо в гру 'Слова'\nЗараз я розкажу тобі правила"<<endl<<endl;
+    cout << "\t Привіт!!\nСьогодні ми зіграємо в гру 'Слова'\nЗараз я розкажу тобі правила"<<endl<<endl;
 
-    //system("pause");
-   // system("cls");
+    system("pause");
+    system("cls");
 
     string bufer = "правила";
-    //rulespintf(bufer);
+    rulespintf(bufer);
 
-    ifstream LOGF("LOGS.txt",ios_base::in);
     system("cls");
     string filename;
 
@@ -53,9 +38,10 @@ int main()
         cout << " Напиши 'міста', щоб вибрати тему гри 'міста України'\n\t'країни', щоб вибрати тему гри 'країни світу (члени ООН)' \n\t'прізвища', щоб вибрати тему 'Прізвища 143 групи'"<<endl;
         getline(cin,bufer);
 
+
         if (exit(bufer) == 1) return 0;
 
-        if ( bufer == "Правила" || bufer == "правила")
+        else if ( bufer == "Правила" || bufer == "правила")
         {
              rulespintf(bufer);
              cout << "Введіть слово:";
@@ -85,19 +71,19 @@ int main()
         else system("cls");
     }
 
-    string Random[50];
-    string line2;
     bufer="";
     char b = '\0';
-
+    int ls_lt; // беремо останню чи передостанню літеру в слові
+    srand ( time(NULL) );
     bool f=0;
     string word;
     label:// СПАГЕТТІ >>> використано goto
     int i;
-    srand(time(NULL));
 
-    cin >> bufer;
+    getline(cin,bufer);
+
     ConverttoLow(bufer);
+
     if (exit(bufer) == 1) return 0;
 
     while(true)//загальний цикл
@@ -113,13 +99,7 @@ int main()
             cout << "Введіть слово - ";
             bufer="";
             goto label;
-            //ваіпаcin >> bufer;
         }
-        else if(searchinLOG(bufer) == 1)
-            {
-                cout<<"Не хитруй!\nЦе слово вже використовувалось."<<endl;
-                goto label;
-            }
         while(rightword(bufer,filename) == 0)
         {
             cout << "Не хитруй!\nЦе не слово або це слово не по темі"<< endl;
@@ -133,48 +113,32 @@ int main()
             }
             goto label;
         }
+        if(searchinLOG(bufer) == 1)
+        {
+            cout<<"Не хитруй!\nЦе слово вже використовувалось."<<endl;
+            goto label;
+        }
 
         setsize=0;
-        //rightword(bufer,filename);
+        ls_lt = 1;
 
-       // while (rightword(bufer,filename) == true)
-        //{
-        LOGAD(bufer);
+        LOGAD(bufer); // Після перевірки правильності введеного слова записуємо його у лог.
+
         b = bufer[bufer.length()-1];//Буква на яку потрібне слово
         if( b == 'ь' || b == 'и' )
         {
+            ls_lt = 2;
             cout<<"Немає слів на - "<<b<<endl;
-            b = bufer[bufer.length()-2];
+            b = bufer[bufer.length() - ls_lt];
         }
         cout<<"Я повинна сказати слово на букву - "<<b<<endl;
         file.open(filename.c_str(),ios_base::in);
         word = "";
+
         i=0;//ітератор
 
-           /* while(!file.eof())
-            {
-                f=1;//добавить слово в обработку
-                file>>word;// нове слово
+        getword(bufer,filename,setsize,word,ls_lt);
 
-                f = !(searchinLOG(word));
-                if( word[0] != b )
-                    f=0;
-                else if(f)
-                {
-                    Random[i] = word;
-                    i++;
-                }
-                else if( i == 100 )
-                {
-                    break;
-                }
-            }*/
-
-        srand(time(NULL));
-
-        getword(bufer,filename,setsize,i,line2);
-
-        word = line2;
         LOGAD(word);
 
         if( word == "end" )
@@ -198,30 +162,23 @@ int main()
         }
         LOG.close();
         LOG.open("LOGS.txt",ios_base::in);
-
         while(true)
         {
             label2:
-            cin>>bufer;
 
-            ConverttoLow(bufer);
-            if (exit(bufer) == 1) return 0;
+            getline(cin,bufer);
 
-            if ( bufer == "правила" )
+            ConverttoLow(bufer); // Перетворення в реєстер малих букв
+
+            if (exit(bufer) == 1) return 0;// Перевірка чи не вихід
+
+            if ( bufer == "правила" )// Перевірка чи не правила
             {
                 rulespintf(bufer);
                 system("cls");
                 bufer="";
                 cout << "Введи слово - ";
-                goto label2;
-            }
-
-            //ConverttoLow(bufer);
-
-            if(searchinLOG(bufer) == 1)
-            {
-                cout<<"Не хитруй!\nЦе слово вже використовувалось."<<endl;
-                continue;
+                goto label2; // Перехід на початок while циклу
             }
 
             if(bufer[0] == b)
@@ -231,14 +188,8 @@ int main()
 
             }
             file.close();
-       // }
-
+            LOG.close();
         }
-        /*cout << "Не хитруй!\nЦе не слово або це слово не по темі222222222"<< endl;
-        if ( b == '\0')
-                cout << "Скажи своє слово - ";
-        else cout << "Скажи слово на букву - "<<b<<endl;
-        goto label;*/
         file.close();
         file.clear();
    return 0;
